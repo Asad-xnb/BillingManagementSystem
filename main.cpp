b39b53c288f20c4d867f9a28f54d7d1c9c4609ea
Asad <asad.xnb@gmail.com>
2024-05-26 14:10:07 +0500

    invoice generation completed

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 #include <cstdlib> // To use rand function
 #include <unistd.h> // To use sleep function
 #include <iomanip> // To use setw function
+#include <ctime> // To use time function
 using namespace std;
 
 struct Item {
-    int id;
+    string id;
     string name;
     int price;
 };
@@ -55,7 +56,10 @@ public:
         }
     }
 
-    void removeItem(const int& id) {
+    void removeItem(const string& id) {
+        if (id == "n" || id == "N") {
+            return;
+        }
         if (head == nullptr) {
             cout << "\t\t\t\tItem not found!\n";
             for (int i = 0; i < 1; i++) {
@@ -95,36 +99,50 @@ public:
     }
 
     void generateInvoice() const {
-        int totalAmount = 0.0;
+        srand(time(0));
+        long long invoiceID = rand() % 9000000000 + 1000000000;
+        int totalAmount = 0;
 
-        cout << "\t\tInvoice:\n";
-        cout << "\t\t-------------------------\n";
+        cout << "\n\n\t\t\t\tInvoice ID: " <<  invoiceID << endl;
+        cout << "\t\t\t\t-------------------------------------\n";
+        cout << "\t\t\t\t| Name           | Price            |\n";
+        cout << "\t\t\t\t-------------------------------------\n";
+        cout << "\t\t\t\t|                                   |\n";
+        cout << "\t\t\t\t|                                   |\n";
 
         Node* current = head;
-        while (current != nullptr) {
-            cout << current->data.name << ": $" << current->data.price << "\n";
+         while (current != nullptr) {
+            cout << "\t\t\t\t| " << setw(15) << left << current->data.name << "  RS: " << setw(12) << fixed << current->data.price << " |\n";
             totalAmount += current->data.price;
             current = current->next;
         }
-
-        cout << "\t\t-------------------------\n";
-        cout << "\t\tTotal amount: $" << totalAmount << "\n";
+        cout << "\t\t\t\t|                                   |\n";
+        cout << "\t\t\t\t|                                   |\n";
+        cout << "\t\t\t\t-------------------------------------\n";
+        cout << "\t\t\t\tTotal amount:              RS: " << setw(10) << fixed << totalAmount << " \n";
+        cout << "\t\t\t\t-------------------------------------\n";
     }
+
+    bool itemsAvailable() {
+        return head != nullptr;
+    }
+
     bool displayAllItems() {
         Node* current = head;
         if (current == nullptr) {
             return false;
+        } else {
+            cout << "\t\t\t\t| ID   | Name           | Price      |\n";
+            cout << "\t\t\t\t-------------------------------------\n";
+            while (current != nullptr) {
+                cout << "\t\t\t\t| " << setw(4) << left << current->data.id << " | "
+                    << setw(14) << left << current->data.name << " | "
+                    << "RS: " << setw(6) << fixed << current->data.price << " |\n";
+                current = current->next;
+            }
+            return true;
+
         }
-        cout << "\n\n\n\n";
-        cout << "\t\t\t\t| ID   | Name           | Price      |\n";
-        cout << "\t\t\t\t-------------------------------------\n";
-        while (current != nullptr) {
-            cout << "\t\t\t\t| " << setw(4) << left << current->data.id << " | "
-                << setw(14) << left << current->data.name << " | "
-                << "RS: " << setw(6) << fixed << current->data.price << " |\n";
-            current = current->next;
-        }
-        return true;
     }
 
 
@@ -139,8 +157,9 @@ void displayMenu() {
     cout << "\t\t\t\t******************************************\n";
     cout << "\t\t\t\t* 1. Add item                            *\n";
     cout << "\t\t\t\t* 2. Remove item                         *\n";
-    cout << "\t\t\t\t* 3. Display items                       *\n";
-    cout << "\t\t\t\t* 4. Generate invoice                    *\n";
+    cout << "\t\t\t\t* 3. Update item                         *\n";
+    cout << "\t\t\t\t* 4. Display items                       *\n";
+    cout << "\t\t\t\t* 5. Generate invoice                    *\n";
     cout << "\t\t\t\t*                                        *\n";
     cout << "\t\t\t\t* 0. Exit                                *\n";
     cout << "\t\t\t\t*                                        *\n";
@@ -173,7 +192,7 @@ int main() {
                 SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN  );
                 system("cls");
                 Item newItem;
-                newItem.id = rand() % 10000 + 1000;
+                newItem.id = to_string(rand() % 10000 + 1000);
                 cout << "\n\n\n\n";
                 cout << "\t\t\t\tEnter item name: ";
                 cin >> newItem.name;
@@ -190,56 +209,128 @@ int main() {
             }
             case 2: {
                 SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE );
-                if(!bill.displayAllItems()) {
-                    cout << "\n\n\t\t\t\tNo items found!\n\n";
-                    cout << "\n\n\t\t\t\t\tEnter (Y) to Get back: ";
-                    char btn;
-                    cin >> btn;
-                    if (btn == 'y' || btn == 'Y') {}
-                    break;
-                }
-                int id;
-                cout << "Enter item ID to remove: ";
-                cin >> id;
-                bill.removeItem(id);
                 system("cls");
-                cout << "\n\n\n\n";
-                cout << "\t\t\t\tThe updated list is: \n\n\n\n";
-                if(!bill.displayAllItems()) {
-                    cout << "\n\n\t\t\t\tOops! - No more Items are left list\n\n";
-                    cout << "\n\n\t\t\t\t\tEnter (Y) to Get back: ";
-                    char btn;
-                    cin >> btn;
-                    if (btn == 'y' || btn == 'Y') {}
+                if(!bill.itemsAvailable()) {
+                    cout << "\n\n\t\t\t\tNo items found!\n\n";
+                    cout << "\n\n\t\t\tPress Enter to continue... ";
+                    cin.ignore();
+                    cin.get();
+                    break;
+                } else {
+                    cout << "\n\n\n\n";
+                    cout << "\t\t\t\tThe current list:\n";
+                    bill.displayAllItems();
+                    string id;
+                    cout << "\n\n\t\t\tEnter item ID to remove or (n) to go back: ";
+                    cin >> id;
+                    bill.removeItem(id); 
+                    system("cls");
+                    cout << "\n\n\n\n";
+                    cout << "\t\t\t\tThe updated list: \n\n\n\n";
+                    if(!bill.itemsAvailable()) {
+                        cout << "\n\n\t\t\t\tOops! - No more Items are left list\n\n";
+                    } else {
+                        bill.displayAllItems();
+                    }
+                    cout << "\n\n\t\t\tPress Enter to continue... ";
+                    cin.ignore();
+                    cin.get();
                     break;
                 }
-                break;
             }
 
             case 3: 
                 system("cls");
-                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN  );
-                if (!bill.displayAllItems()) {
+                cout << "\n\n\n\t\t\t\t\tLoading";
+                for (int i = 0; i < 2; i++) {
+                    cout << ".";
+                    sleep(1);
+                }
+                system("cls");
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN  );
+                if(!bill.itemsAvailable()) {
                     cout << "\n\n\t\t\t\tNo items found!\n\n";
-                    cout << "\t\t\t\t\tEnter (Y) to Get back: ";
-                    char btn;
-                    cin >> btn;
-                    if (btn == 'y' || btn == 'Y') {}
+                    cout << "\n\n\t\t\tPress Enter to continue... ";
+                    cin.ignore();
+                    cin.get();
+                    break;
+                } else {
+                    cout << "\n\n\n\n";
+                    cout << "\t\t\t\tThe current list:\n";
+                    bill.displayAllItems();
+                    string id;
+                    cout << "\n\n\t\t\tEnter item ID to update or (n) to go back: ";
+                    cin >> id;
+                    bill.removeItem(id);
+                    Item newItem;
+                    newItem.id = id;
+                    cout << "\n\n\t\t\t\tEnter new item name: ";
+                    cin >> newItem.name;
+
+                    cout << "\t\t\t\tEnter new item price: ";
+                    cin >> newItem.price;
+                    cout << "\t\t\t\t\tLoading";
+                    for (int i = 0; i < 2; i++) {
+                        cout << ".";
+                        sleep(1);
+                    }
+                    bill.addItem(newItem);
                     break;
                 }
-                cout << "\n\n\t\t\t\t\tEnter (Y) to Get back: ";
-                char btn;
-                cin >> btn;
-                if (btn == 'y' || btn == 'Y') {}
-                break;
-            case 4:
-                bill.generateInvoice();
-                break;
+            case 4: 
+                system("cls");
+                cout << "\n\n\n\t\t\t\t\tLoading";
+                for (int i = 0; i < 2; i++) {
+                    cout << ".";
+                    sleep(1);
+                }
+                system("cls");
+                SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE  );
+                if (!bill.itemsAvailable()) {
+                    cout << "\n\n\t\t\t\tNo items found!\n\n";
+                    cout << "\n\n\t\t\tPress Enter to continue... ";
+                    cin.ignore();
+                    cin.get();
+                    break;
+                } else {
+                    cout << "\n\n\n\n";
+                    bill.displayAllItems();
+                    cout << "\n\n\t\t\tPress Enter to continue... ";
+                    cin.ignore();
+                    cin.get();
+                    break;
+                }
+                    
+            case 5:
+                system("cls");
+                cout << "\n\n\n\t\t\t\t\tLoading";
+                for (int i = 0; i < 2; i++) {
+                    cout << ".";
+                    sleep(1);
+                }
+                system("cls");
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN  );
+                if(!bill.itemsAvailable()) {
+                    cout << "\n\n\t\t\t\tNo items found!\n\n";
+                    cout << "\n\n\t\t\tPress Enter to continue... ";
+                    cin.ignore();
+                    cin.get();
+                    break;
+                } else {
+                    bill.generateInvoice();
+                    cout << "\n\n\t\t\tPress Enter to continue... ";
+                    cin.ignore();
+                    cin.get();
+                    break;
+                }
             case 0:
-                cout << "Exiting...\n";
+                cout << "\n\n\n\n\t\t\tExiting...\n";
                 break;
             default:
-                cout << "Invalid choice! Please try again.\n";
+                cout << "\n\n\n\n\t\t\tInvalid choice! Please try again.\n";
+                cout << "\n\n\t\t\tPress Enter to continue... ";
+                cin.ignore();
+                cin.get();
                 break;
         }
     } while (choice != 0);
